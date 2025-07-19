64ede1fa4eae8fd9a46d6402cffa769cabb6258c
sendosend <johnsenberdin2930@gmail.com>
2025-07-19 15:55:38 +0800

    Final refactor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const gridContainer = document.querySelector(".grid-container");
 const button = document.querySelector("button");
-
+const randomColor = ``;
 const createGridSize = (size) => {
   const squareSize = 960 / size;
   for (let i = 0; i < size * size; i++) {
@@ -10,7 +10,18 @@ const createGridSize = (size) => {
     div.style.height = `${squareSize}px`;
 
     div.addEventListener("mouseenter", () => {
-      div.style.backgroundColor = "#4CAF50";
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      const randomColor = `rgb(${r}, ${g}, ${b})`;
+      div.style.backgroundColor = randomColor;
+
+      const darkness = parseFloat(div.dataset.darkness);
+      if (darkness < 1) {
+        darkness += 0.1;
+        div.dataset.darkness = darkness.toFixed(1);
+        div.style.filter = `brightness(${1 - darkness})`;
+      }
     });
 
     div.addEventListener("mouseleave", () => {
